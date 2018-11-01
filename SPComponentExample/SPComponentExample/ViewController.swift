1a3e10e8b7ca11656a420c69041016277d18d561
Runryan <mob_developer@163.com>
2018-11-01 11:59:51 +0800

    MJRefresh下拉控件封装，用一个接口同时监听下拉刷新和上拉加载更多事件； 使用一个接口处理下拉和加载更多完成后的状态 不允许同时下拉和上拉这种奇葩的操作

--- SPComponentExample/SPComponentExample/ViewController.swift
+++ SPComponentExample/SPComponentExample/ViewController.swift
@@ -10,18 +10,99 @@ import UIKit
 import MJRefresh
 import SnapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    private let cellID = "cellID"
+    private weak var tableView: UITableView?
+    private var numberOfRows = 20
+    private var refreshControl: SPPullToRefreshControl<UITableView>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let tableView = UITableView()
-        view.addSubview(tableView)
-        tableView.snp.makeConstraints { make in
-            make.edges.equalTo(self.view)
-        }
-        
+        setupTableView()
+        setupPullToRefresh()
+        setupDecorationView()
     }
     
+    func doRefreshWork(isRefresh: Bool) {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 1) {[unowned self] in
+            DispatchQueue.main.sync {
+                if isRefresh {
+                    self.numberOfRows = 0
+//                    self.tableView?.mj_footer.isHidden = false
+//                    self.tableView?.mj_header.endRefreshing()
+//                    self.tableView?.reloadData()
+                    print("数据刷新成功")
+                } else {
+                    self.numberOfRows = self.numberOfRows + 10
+                    print("加载了更多数据")
+                }
+                self.refreshControl?.loadDataCompleted()
+            }
+        }
+    }
+    
+    func setupPullToRefresh() {
+        let pullTorefreshControll = SPPullToRefreshControl(scrollView: self.tableView!)
+        self.refreshControl = pullTorefreshControll
+        self.refreshControl?.loadDataBlock = { [weak self] refreshType in
+            switch refreshType {
+            case .refresh:
+                self?.doRefreshWork(isRefresh: true)
+            case .loadMore:
+                self?.doRefreshWork(isRefresh: false)
+            }
+        }
+    }
+    
+    func setupTableView() {
+        let tableView = UITableView()
+        self.tableView = tableView
+        tableView.backgroundColor = .red
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            if #available(iOS 11.0, *) {
+                make.edges.equalTo(self.view.safeAreaLayoutGuide)
+            } else {
+                make.bottom.equalTo(self.view)
+            }
+        }
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+    }
+    
+    func setupDecorationView() {
+        if #available(iOS 11.0, *) {
+            let bottomView = UIView()
+            bottomView.backgroundColor = .green
+            view.addSubview(bottomView)
+            bottomView.snp.makeConstraints { make in
+                make.left.right.equalTo(self.view.safeAreaLayoutGuide)
+                make.top.equalTo(self.view.safeAreaLayoutGuide.snp.bottom)
+                make.bottom.equalTo(self.view)
+            }
+            
+            let topView = UIView()
+            topView.backgroundColor = .yellow
+            view.addSubview(topView)
+            topView.snp.makeConstraints { make in
+                make.left.right.equalTo(self.view.safeAreaLayoutGuide)
+                make.top.equalTo(self.view)
+                make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfRows
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID)!
+        cell.textLabel?.text = "第\(indexPath.row)行"
+        return cell
+    }
     
 }
 
